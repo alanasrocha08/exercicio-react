b3a23c3e9a0917ef866e5410739657096d27ff35
alanasrocha08 <alanasr02@gmail.com>
2025-07-28 09:42:36 -0300

    .

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Exercício React</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Ballet "Giselle"</a>
+        <a href="">Ballet "Dom Quixote"</a>
+        <a href="">Ballet "O Lago dos Cisnes"</a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Conteudo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Seja bem-vindo(a) aomundo do Ballet!</h2>
+        <p>
+          Explore os grandes clássicos do ballet e descubra obras que marcaram a
+          história da dança.
+        </p>
+
+        <p>
+          Navegue pelos links ao lado para conhecer mais sobre balés como
+          Giselle, Dom Quixote, O Lago dos Cisnes e outros ícones do repertório
+          mundial.
+        </p>
+      </section>
+    </main>
+  );
+}
