ddc756c19a25c3c125e8e107fe2793f2c8a29413
alanasrocha08 <alanasr02@gmail.com>
2025-07-28 09:51:02 -0300

    .

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,14 +1,11 @@
+import Menu from "./Menu";
+
 export default function Cabecalho() {
   return (
     <header>
       <h1>Exercício React</h1>
       <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Ballet "Giselle"</a>
-        <a href="">Ballet "Dom Quixote"</a>
-        <a href="">Ballet "O Lago dos Cisnes"</a>
-      </nav>
+      <Menu />
     </header>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+export default function Menu() {
+  return (
+    <nav>
+      <a href="">Home</a>
+      <a href="">Ballet "Giselle"</a>
+      <a href="">Ballet "Dom Quixote"</a>
+      <a href="">Ballet "O Lago dos Cisnes"</a>
+    </nav>
+  );
+}
