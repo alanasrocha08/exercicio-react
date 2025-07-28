c3ab3615d54bd6aa3e48a8ce84b22fc188b29fc0
alanasrocha08 <alanasr02@gmail.com>
2025-07-28 10:43:31 -0300

    .

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,6 +1,15 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "#fffff",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
-    <footer>
+    <footer style={estilosRodape}>
       <h2>Pequeno site criado com React</h2>
       <p>
         Desenvolvido por <b>Alana</b> &copy; 2025
